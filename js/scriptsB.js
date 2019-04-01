e485f718f00f18349a14703c8d46e59a5bf7921f
Tessa Sullivan and Zach Weintraub <tessa@>
2019-04-01 14:47:26 -0700

    add output in descending order, dry up the code

--- js/scriptsB.js
+++ js/scriptsB.js
@@ -13,16 +13,30 @@ function findUniques(sentence) {
   return wordList;
 };
 
+function sortObject(ourObject) {
+  var arrayArray = Object.entries(ourObject);
+  arrayArray.sort(function(a, b){
+    return b[1] - a[1];
+  });
+  return arrayArray;
+};
+
 $(function(){
   $('#sentenceForm').submit(function(event){
     event.preventDefault();
-    var wordList = findUniques($('#sentence').val());
+    var wordList = sortObject(findUniques($('#sentence').val()));
     console.log(wordList);
     $('#output').append("<ul></ul>");
+    wordList.forEach(function(word){
+      $('#output ul').append('<li>' + word[0] + ': ' + word[1] + '</li>');
+    });
 
-    for (var word in wordList) {
-      $('#output ul').append("<li>" + word + ": " + wordList[word] + "</li>");
-    }
+
+
+
+    // for (var word in wordList) {
+    //   $('#output ul').append("<li>" + word + ": " + wordList[word] + "</li>");
+    // }
 
   });
 
